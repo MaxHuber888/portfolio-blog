609b686710db018b2f68716ae2b66e7f14c55fe5
Max Huber <huber.maxi@northeastern.edu>
2024-08-21 18:09:56 -0400

    Updated home page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
         <p className="mb-4">
             {`I decided to try and start a personal blog to improve my writing abilities (without the help of generative AI).I will definitely write about AI/ML and other tech-related topics, but I also want to try 
-            writing about my other interests: meditation, spirituality, movies, music, environmentalism, 
+            writing about my other interests: art, mindfulness, spirituality, movies, music, biology, physics, 
             and whatever else I feel inspired by.`}
         </p>
         <h2 className="font-semibold text-xl mb-4 tracking-tighter">Blog Posts:</h2>
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,46 @@
 import Image from 'next/image';
-import robotImage from './public/robot.png';
+import Contact from './components/contact';
 
 export default function Page() {
-  return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        About Me
-      </h1>
-        <p className="mb-4">
-            {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-        </p>
-      <Image
-        src={robotImage}
-        alt="a silly robot icon"
-        width={200}
-        height={200}
-        className="mx-auto block my-8"
-        />
-      <h2 className="mb-4 font-semibold tracking-tighter">Contact Me:</h2>
-        <ul id={"list"}>
-            <li>Email: <a href="mailto:huber.maxi@northeastern.edu" className="dark:hover:text-neutral-400">huber.maxi@northeastern.edu</a></li>
-            <li>GitHub: <a href="https://github.com/MaxHuber888" className="dark:hover:text-neutral-400">@MaxHuber888</a></li>
-            <li>LinkedIn: <a href="https://www.linkedin.com/in/max-huber888/" className="dark:hover:text-neutral-400">Maximilian Huber</a></li>
-            <li>Twitter: <a href="https://twitter.com/MaxHuber888" className="dark:hover:text-neutral-400">@maxhuber888</a></li>
-        </ul>
-    </section>
-  )
+    return (
+      <section>
+          <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+              About Me
+          </h1>
+          <div className="flex justify-center space-x-4 my-8">
+              <Image
+                  src="/images/max.png"
+                  alt="Max Huber headshot photo"
+                  width={200}
+                  height={200}
+                  className="mx-auto block my-8"
+              />
+              <Image
+                  src="/images/robot.png"
+                  alt="a silly robot icon"
+                  width={200}
+                  height={200}
+                  className="mx-auto block my-8"
+              />
+          </div>
+          <p className="mb-4">
+              Hi, I’m Max Huber. I’m a CS student and researcher with a focus on AI and a curiosity that
+              extends beyond algorithms.
+          </p>
+          <p className="mb-4">
+              I recently completed my Bachelor's Degree in Computer Science at Northeastern University and I’m
+              now pursuing a Master’s Degree in AI at Northeastern.
+          </p>
+          <p className="mb-4">
+              <strong className={"color-changing-text"}> I am also seeking a Spring 2025 co-op in AI, data engineering, or software development! </strong>
+          </p>
+          <Contact />
+          <p className="mb-4">
+              This site is a place where I will share both my professional and personal projects and ideas. I love to create
+              both in technical fields and creative mediums. Check out the <a href="/blog" className={"dark:hover:text-neutral-400"}>blog</a> page to read about ideas
+              that inspire me, and visit the <a href="/projects" className={"dark:hover:text-neutral-400"}>projects</a> page for a list of all of my AI, software development, game
+              development, and graphic design projects.
+          </p>
+      </section>
+    );
 }
