ae4fd07b4fe78eb6017e81d2f0328b5c3032f071
Max Huber <huber.maxi@northeastern.edu>
2024-10-20 17:40:26 -0400

    Updated projects with emoji fields and changed colors of status

--- app/components/projects.tsx
+++ app/components/projects.tsx
@@ -15,25 +15,48 @@ export function Projects({ projType }: { projType: string }) {
           }
           return 1;
         })
-        .map((post) => (
-          <Link
-            key={post.slug}
-            className="flex border flex-col space-y-1 mb-4 group"
-            href={`/projects/${post.slug}`}
-          >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight group-hover:text-neutral-400">
-                <b>{post.metadata.title}</b>
-              </p>
-              <p className="text-neutral-500 dark:text-neutral-400 text-sm group-hover:text-neutral-600">
-                [{post.metadata.status}]
-              </p>
-            </div>
-            <p className="text-neutral-500 dark:text-neutral-400 text-sm group-hover:text-neutral-600">
-              {post.metadata.summary}
-            </p>
-          </Link>
-        ))}
+        .map((post) => {
+          return (
+            <Link
+              key={post.slug}
+              className="flex border flex-col space-y-1 mb-4 group"
+              href={`/projects/${post.slug}`}
+            >
+              <div className="w-full flex items-center space-x-4">
+                <div className="flex items-center justify-center min-w-[40px]">
+                  <span
+                    className="text-3xl"
+                    role="img"
+                    aria-label="project emoji"
+                  >
+                    {post.metadata.emoji || "✨"}
+                  </span>
+                </div>
+                <div className="flex-1">
+                  <div className="flex flex-col md:flex-row md:items-center space-y-1 md:space-y-0 md:space-x-2">
+                    <p className="text-neutral-900 dark:text-neutral-100 tracking-tight group-hover:text-neutral-400">
+                      <b>{post.metadata.title}</b>
+                    </p>
+                    <p
+                      className={`text-sm group-hover:text-neutral-600 ${
+                        post.metadata.status === "CLOSED"
+                          ? "text-[#F92A82]"
+                          : post.metadata.status === "ACTIVE"
+                            ? "text-[#00A99D]"
+                            : "text-[#7692FF]"
+                      }`}
+                    >
+                      [{post.metadata.status}]
+                    </p>
+                  </div>
+                  <p className="text-neutral-500 dark:text-neutral-400 text-sm">
+                    {post.metadata.summary}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,32 @@
-import './global.css'
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
-import { baseUrl } from './sitemap'
+import "./global.css";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Navbar } from "./components/nav";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Footer from "./components/footer";
+import { baseUrl } from "./sitemap";
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Max Huber Portfolio',
-    template: '%s | Max Huber Portfolio',
+    default: "Max Huber Portfolio",
+    template: "%s | Max Huber Portfolio",
   },
-  description: 'This is my portfolio.',
+  description: "A collection of my projects and my personal blog.",
   openGraph: {
-    title: 'My Portfolio',
-    description: 'This is my portfolio.',
+    title: "Max Huber Portfolio",
+    description: "A collection of my projects and my personal blog.",
     url: baseUrl,
-    siteName: 'My Portfolio',
-    locale: 'en_US',
-    type: 'website',
+    siteName: "Max Huber Portfolio",
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "/images/robot.png",
+      },
+    ],
   },
   robots: {
     index: true,
@@ -29,27 +34,27 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
-}
+};
 
-const cx = (...classes) => classes.filter(Boolean).join(' ')
+const cx = (...classes) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        "text-black bg-white dark:text-white dark:bg-black",
         GeistSans.variable,
-        GeistMono.variable
+        GeistMono.variable,
       )}
     >
       <body className="antialiased max-w-2xl mx-4 mt-8 lg:mx-auto">
@@ -62,5 +67,5 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  )
+  );
 }
