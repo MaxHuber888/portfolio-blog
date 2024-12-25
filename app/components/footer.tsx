ac1b022a918e944eb55e76c5946a8f367489c879
Max Huber <huber.maxi@northeastern.edu>
2024-12-25 14:53:31 -0500

    removed kofi button

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -19,16 +19,6 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:items-center md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li className="flex items-center">
-          <a href="https://ko-fi.com/R6R712Z8XF" target="_blank">
-            <img
-              height="36px"
-              style={{ height: "36px" }}
-              src="https://storage.ko-fi.com/cdn/kofi5.png?v=3"
-              alt="Buy Me a Coffee at ko-fi.com"
-            />
-          </a>
-        </li>
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
