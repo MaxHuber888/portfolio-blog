f34005bab275df82100ea31d9df6e3783745d802
Max Huber <huber.maxi@northeastern.edu>
2024-12-26 01:15:06 -0500

    First blog post!

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -5,30 +5,31 @@ export function BlogPosts() {
   let allBlogs = getBlogPosts()
 
   return (
-    <div>
+    <div className="flex flex-col space-y-4">
       {allBlogs
         .sort((a, b) => {
-          if (
-            new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
-          ) {
-            return -1
+          if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
+            return -1;
           }
-          return 1
+          return 1;
         })
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col space-y-2 mb-4 border"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[200px] tabular-nums">
+            <div className="w-full flex justify-between items-center">
+              <p className="text-neutral-900 dark:hover:text-neutral-400 dark:text-neutral-100 tracking-tight">
+                <b>{post.metadata.title}</b>
+              </p>
+              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-neutral-900 dark:hover:text-neutral-400 dark:text-neutral-100 tracking-tight">
-                {post.metadata.title}
-              </p>
             </div>
+            <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+              {post.metadata.summary}
+            </p>
           </Link>
         ))}
     </div>
